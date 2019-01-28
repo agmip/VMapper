d40b66785104e66ecea9bb2a6f86052b7f42ba23
MengZhang <meng.zhang15@ufl.edu>
2019-01-27 19:27:16 -0500

    remove debug code

--- src/main/resources/public/js/plot/Heatmap.js
+++ src/main/resources/public/js/plot/Heatmap.js
@@ -1,9 +1,6 @@
 /* global Highcharts */
 
 function drawDailyHeatMapPlot(plotVar, plotVarName, data, soilProfile, containerId, day, zoom) {
-//    let plotTitle = "Sales per employee per weekday";
-//    let plotValTitle = "Sales per employee";
-//    let plotData = [[0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84], [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 48], [9, 4, 91]];
     let daily = data["daily"];
     let max = data["max"][plotVar];
     let min = data["min"][plotVar];
@@ -26,7 +23,7 @@ function drawDailyHeatMapPlot(plotVar, plotVarName, data, soilProfile, container
         }
     }
     
-    console.log("max:" + max + " min:" + min + " average:" + avg + " median:" + med + " color zoom:" + colorZoom)
+//    console.log("max:" + max + " min:" + min + " average:" + avg + " median:" + med + " color zoom:" + colorZoom)
     
     drawHeatMapPlot(plotTitle, plotValTitle, plotData, max, min, soilProfile, containerId, colorZoom, zoom);
 }
@@ -48,22 +45,11 @@ function drawHeatMapPlot(plotTitle, plotValTitle, plotData, max, min, soilProfil
             text: plotTitle
         },
 
-//        xAxis: {
-//            categories: ['Alexander', 'Marie', 'Maximilian', 'Sophia', 'Lukas', 'Maria', 'Leon', 'Anna', 'Tim', 'Laura']
-//        },
-//
-//        yAxis: {
-//            categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-//            title: null
-//        },
-
-
         xAxis: {
             min: 1,
             softMax: soilProfile["totCols"],
-            gridLineWidth: 1,
             tickInterval: 1,
-            gridLineWidth:0,
+            gridLineWidth: 0,
             title: {
                 text: 'Soil Column',
                 align: 'low'
@@ -73,8 +59,8 @@ function drawHeatMapPlot(plotTitle, plotValTitle, plotData, max, min, soilProfil
             min: 1,
             softMax: soilProfile["totRows"],
             tickInterval: 1,
-            reversed: true,
             gridLineWidth:0,
+            reversed: true,
             title: {
                 text: 'Soil layers',
                 align: 'low'
