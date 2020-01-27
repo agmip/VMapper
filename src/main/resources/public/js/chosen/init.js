9f4a8f02a86808168c1fb4a940d7e3bf965707f3
MengZhang <meng.zhang15@ufl.edu>
2020-01-27 14:46:27 -0500

    revise init method for bootbox componenet

--- src/main/resources/public/js/chosen/init.js
+++ src/main/resources/public/js/chosen/init.js
@@ -9,43 +9,27 @@ let chonsenConfigs = {
   'chosen-select-width'     : { width: '95%' }
 };
 
-function chosen_init_all() {
-    for (let selector in chonsenConfigs) {
-        $("."+selector).chosen("destroy");
-        $("."+selector).chosen(chonsenConfigs[selector]);
+function chosen_init_all(container) {
+    if (container) {
+        for (let selector in chonsenConfigs) {
+            container.find("."+selector).chosen("destroy");
+            container.find("."+selector).chosen(chonsenConfigs[selector]);
+        }
+    } else {
+        for (let selector in chonsenConfigs) {
+            $("."+selector).chosen("destroy");
+            $("."+selector).chosen(chonsenConfigs[selector]);
+        }
     }
+    
 }
 
 function chosen_init(id, className) {
-    let target = $("#"+id);
-    target.chosen("destroy");
-    if (className === undefined) {
-        for (let selector in chonsenConfigs) {
-            if (target.hasClass(selector)) {
-                target.chosen(chonsenConfigs[selector]);
-                return;
-            }
-        }
-        target.chosen(chonsenConfigs["chosen-select"]);
-    } else {
-        target.chosen(chonsenConfigs[className]);
-    }
+    chosen_init_target($("#"+id), className);
 }
 
 function chosen_init_name(name, className) {
-    let target = $("[name="+name+"]");
-    target.chosen("destroy");
-    if (className === undefined) {
-        for (let selector in chonsenConfigs) {
-            if (target.hasClass(selector)) {
-                target.chosen(chonsenConfigs[selector]);
-                return;
-            }
-        }
-        target.chosen(chonsenConfigs["chosen-select"]);
-    } else {
-        target.chosen(chonsenConfigs[className]);
-    }
+    chosen_init_target($("[name="+name+"]"), className);
 }
 
 function chosen_init_target(target, className) {
