53787fe14d0a3cb081de522447066ddfa54775b3
MengZhang <meng.zhang15@ufl.edu>
2020-05-15 20:11:15 -0400

    minor revise

--- src/main/resources/public/js/chosen/init.js
+++ src/main/resources/public/js/chosen/init.js
@@ -1,12 +1,12 @@
 let chonsenConfigs = {
-  'chosen-select'           : {},
+  'chosen-select'           : {search_contains: true},
   'chosen-select-max4'       : { max_selected_options : 4, search_contains: true},
   'chosen-select-deselect'  : { allow_single_deselect: true, search_contains: true },
   'chosen-select-deselect-single'  : { allow_single_deselect: true, max_selected_options : 1, search_contains: true },
-  'chosen-select-no-single' : { disable_search_threshold: 10 },
-  'chosen-select-no-results': { no_results_text: 'Oops, nothing found!' },
-  'chosen-select-rtl'       : { rtl: true },
-  'chosen-select-width'     : { width: '95%' }
+  'chosen-select-no-single' : { disable_search_threshold: 10, search_contains: true},
+  'chosen-select-no-results': { no_results_text: 'Oops, nothing found!', search_contains: true},
+  'chosen-select-rtl'       : { rtl: true, search_contains: true},
+  'chosen-select-width'     : { width: '95%', search_contains: true}
 };
 
 function chosen_init_all(container) {
