a64e36ae26499a07ed472c1cb5214ac53b944a9c
MengZhang <meng.zhang15@ufl.edu>
2020-07-02 02:02:20 -0400

    test issue "CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource."

--- src/main/java/org/dssat/tool/gbuilder2d/Main.java
+++ src/main/java/org/dssat/tool/gbuilder2d/Main.java
@@ -23,7 +23,9 @@ import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.Spark;
+import static spark.Spark.before;
 import static spark.Spark.get;
+import static spark.Spark.options;
 import static spark.Spark.port;
 import static spark.Spark.post;
 import static spark.Spark.staticFiles;
@@ -88,6 +90,26 @@ public class Main {
         staticFiles.location("/public");
         staticFiles.expireTime(600L);
         Spark.webSocketIdleTimeoutMillis(60000);
+        options("/*",
+        (request, response) -> {
+
+            String accessControlRequestHeaders = request
+                    .headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers",
+                        accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request
+                    .headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods",
+                        accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
 
         // Set up before-filters (called before each get/post)
 //        before("*",                  Filters.addTrailingSlashes);
